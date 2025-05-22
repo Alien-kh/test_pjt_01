a5cefc74cb4fe7268e4d76f624a88e51462319c5
alien <ghwh323232@naver.com>
2025-05-22 14:22:01 +0900

    start

---
+++ vue-project/src/views/SignUpView.vue
@@ -0,0 +1,46 @@
+<!-- views/SignUpView.vue -->
+
+<template>
+  <div>
+    <form @submit.prevent="onSignUp">
+      <label for="username">username: </label>
+      <input type="text" id="username" v-model="username">
+
+      <label for="password1">password1: </label>
+      <input type="password" id="password1" v-model="password1">
+
+      <label for="password2">password2: </label>
+      <input type="password" id="password2" v-model="password2">
+      <label for="age">age: </label>
+      <input type="number" id="age" v-model="age">
+
+      <input type="submit" value="signup">
+    </form>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  import { useAccountStore } from '@/stores/accounts.js'
+  
+  const accountStore = useAccountStore()
+
+  const username = ref('')
+  const password1 = ref('')
+  const password2 = ref('')
+  const age = ref(null)
+  const onSignUp = function () {
+    const userInfo = {
+      username: username.value,
+      password1: password1.value,
+      password2: password2.value,
+      age: age.value
+    }
+    accountStore.signUp(userInfo)  
+  }
+  
+</script>
+
+<style>
+
+</style>
